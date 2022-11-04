07630893077c5fe67354fd7c5167e21395462c23
nounou NOUNOU <brathebow@gmail.com>
2022-11-04 15:48:15 +0100

    view login in user data

--- angular-vape-T/src/app/app.component.ts
+++ angular-vape-T/src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
+ user !: any;
+ isAvailable !: any;
+
+ constructor(private router: Router, 
+  ){ }
+ 
+ ngOnInit() {
+    this.isAvailable = true;
+    // @ts-ignore
+    this.user = JSON.parse(localStorage.getItem('userData'));
+    console.log(this.user);
+  };
+
+  logOut(){
+    this.isAvailable = false;
+    localStorage.removeItem('userData');
+    alert("You are logged out");
+  }
+  logIn(){
+    
+    this.router.navigate(['/login']);
+   
+  }
   
 }
